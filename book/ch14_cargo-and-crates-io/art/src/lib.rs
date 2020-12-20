7c0ab7d99e8ba9fd7d79e259a2e8704ceaa7ba92
yuichi-morisaki <yuichi.morisaki@gmail.com>
2020-12-20 11:14:18 +0900

    Add sample code in chapter 14

---
+++ book/ch14_cargo-and-crates-io/art/src/lib.rs
@@ -0,0 +1,43 @@
+//! # Art
+//!
+//! A library for modeling artistic concepts.
+
+pub use self::kinds::PrimaryColor;
+pub use self::kinds::SecondaryColor;
+pub use self::utils::mix;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
+
+pub mod kinds {
+    /// The primary colors according to the RYB color model.
+    pub enum PrimaryColor {
+        Red,
+        Yellow,
+        Blue,
+    }
+
+    /// The secondary colors according to the RYB color model.
+    pub enum SecondaryColor {
+        Orange,
+        Green,
+        Purple,
+    }
+}
+
+pub mod utils {
+    use crate::kinds::*;
+
+    /// Combines two primary colors in equal amounts
+    /// to create a secondary color.
+    #[allow(unused_variables)]
+    pub fn mix(c1: PrimaryColor, c2: PrimaryColor) -> SecondaryColor {
+        // --snip--
+        SecondaryColor::Orange
+    }
+}
